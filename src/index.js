819fedd7291d13a4569605a03a20e70c3ab4c108
TheLance <98957643+LanceMinecraftFBA@users.noreply.github.com>
2023-04-25 14:26:06 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,17 +6,21 @@ getImage = function (){
     return imageNum;
 }
 
+closeAlert = function() {
+    document.getElementsByClassName("cookie-alert-container")[0].remove();
+}
+
 alertCookie = function(){
     var date = new Date();
     var cookie_svg_type = [ "morning", "day", "evening" ];
     if(date.getHours() >= 8 && date.getHours() < 12){
-        return "<img class=\"close-button\" src=\"svg/close-buttons-icons/close-alerts-button.svg\" wight=\"15\" height=\"15\"/><h1>Cookie файлы<img src=\"svg/cookie-icon/cookie-" + cookie_svg_type[0] + ".svg\" width=\"35\" height=\"35\" align=\"center\"></h1>\n<h3>Для корректной работы сайта рекомендуем разрешить сохранение куки файлов</h3>";
+        return "<div id=\"cookie-alert\">\n<h1>Cookie файлы<img src=\"svg/cookie-icon/cookie-" + cookie_svg_type[0] + ".svg\" width=\"35\" height=\"35\" align=\"center\"></h1>\n<h3>Для корректной работы сайта рекомендуем разрешить сохранение куки файлов</h3>\n</div>\n<img href=\"#\" id=\"close-button\" src=\"svg/close-buttons-icons/close-alerts-button.svg\" wight=\"15\" height=\"15\"/>";
     }
     else if(date.getHours() >= 12 && date.getHours() <= 18){
-        return "<img class=\"close-button\" src=\"svg/close-buttons-icons/close-alerts-button.svg\" wight=\"15\" height=\"15\"/><h1>Cookie файлы<img src=\"svg/cookie-icon/cookie-" + cookie_svg_type[1] + ".svg\" width=\"35\" height=\"35\" align=\"center\"></h1>\n<h3>Для корректной работы сайта рекомендуем разрешить сохранение куки файлов</h3>";
+        return "<div id=\"cookie-alert\">\n<h1>Cookie файлы<img src=\"svg/cookie-icon/cookie-" + cookie_svg_type[1] + ".svg\" width=\"35\" height=\"35\" align=\"center\"></h1>\n<h3>Для корректной работы сайта рекомендуем разрешить сохранение куки файлов</h3>\n</div>\n<img href=\"#\" id=\"close-button\" src=\"svg/close-buttons-icons/close-alerts-button.svg\" wight=\"15\" height=\"15\"/>";
     }
     else{
-        return "<img class=\"close-button\" src=\"svg/close-buttons-icons/close-alerts-button.svg\" wight=\"15\" height=\"15\"/><h1>Cookie файлы<img src=\"svg/cookie-icon/cookie-" + cookie_svg_type[2] + ".svg\" width=\"35\" height=\"35\" align=\"center\"></h1>\n<h3>Для корректной работы сайта рекомендуем разрешить сохранение куки файлов</h3>";
+        return "<div id=\"cookie-alert\">\n<h1>Cookie файлы<img src=\"svg/cookie-icon/cookie-" + cookie_svg_type[2] + ".svg\" width=\"35\" height=\"35\" align=\"center\"></h1>\n<h3>Для корректной работы сайта рекомендуем разрешить сохранение куки файлов</h3>\n</div>\n<img href=\"#\" id=\"close-button\" src=\"svg/close-buttons-icons/close-alerts-button.svg\" wight=\"15\" height=\"15\"/>";
     }
 }
 
@@ -26,8 +30,11 @@ document.getElementById("background").innerHTML = "<img src=backgrounds/" + numb
 
 if (navigator.cookieEnabled === false){
     document.getElementById("profile-button").innerHTML = "<button><b>Войти</b></button>";
-    document.getElementById("cookie-alert").innerHTML = alertCookie();
+    document.getElementsByClassName("cookie-alert-container")[0].innerHTML = alertCookie();
+    document.getElementById("close-button").addEventListener("click", closeAlert)
 }
+
 document.getElementById("profile-button").innerHTML = "<button><b>Войти</b></button>";
 
-document.getElementById("cookie-alert").innerHTML = alertCookie();
+document.getElementsByClassName("cookie-alert-container")[0].innerHTML = alertCookie();
+document.getElementById("close-button").addEventListener("click", closeAlert);
